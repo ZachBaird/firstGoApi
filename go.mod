f03ac55fd380eb754ed96ee35b2760b1db235581
Zach <zdb1994@yahoo.com>
2023-12-30 22:24:22 -0500

    Adds json serialization and scany.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module firstDbApi
 go 1.21
 
 require (
+	github.com/georgysavva/scany/v2 v2.0.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"github.com/georgysavva/scany/v2/sqlscan"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 	"log"
@@ -15,18 +18,20 @@ const port string = "5432"
 const sslMode string = "disable"
 
 type Lesson struct {
-	id          int
-	title       string
-	description string
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
 
+// createConnString combines package-level variables and environment vars to build a postgresql conn string.
 func createConnString() string {
 	user := os.Getenv("DATABASE_USER")
 	pw := os.Getenv("DATABASE_PASSWORD")
 	return fmt.Sprintf("user=%s port=%s password=%s dbname=%s sslmode=%s", user, port, pw, dbName, sslMode)
 }
 
-func GetLessons(w http.ResponseWriter, r *http.Request) {
+// getLessons queries the db and populates the results in a slice of Lesson.
+func getLessons(w http.ResponseWriter, r *http.Request) {
 	connString := createConnString()
 	db, err := sql.Open("postgres", connString)
 	if err != nil {
@@ -54,7 +59,7 @@ func GetLessons(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		var lesson Lesson
 
-		if err := rows.Scan(&lesson.id, &lesson.title, &lesson.description); err != nil {
+		if err := rows.Scan(&lesson.Id, &lesson.Title, &lesson.Description); err != nil {
 			log.Fatal("something went wrong scanning data")
 		}
 
@@ -62,17 +67,62 @@ func GetLessons(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, lesson := range lessons {
-		fmt.Fprintf(w, "%d: %s - %s\n", lesson.id, lesson.title, lesson.description)
+		fmt.Fprintf(w, "%d: %s - %s\n", lesson.Id, lesson.Title, lesson.Description)
 	}
 
-	db.Close()
+	if err := db.Close(); err != nil {
+		log.Fatal("something went wrong closing db conn")
+	}
+}
+
+// getLessonById queries the db for a Lesson matching the id.
+func getLessonById(w http.ResponseWriter, r *http.Request) {
+	// Declare scany context.
+	ctx := context.Background()
+	connString := createConnString()
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal("the db connection failed")
+	}
+
+	var lesson Lesson
+	vals := mux.Vars(r)
+
+	query := fmt.Sprintf(`SELECT id, title, description fROM lessons WHERE id = %s`, vals["id"])
+	if err := sqlscan.Get(ctx, db, &lesson, query); err != nil {
+		log.Fatal("failed to query db")
+	}
+
+	if err := db.Close(); err != nil {
+		log.Fatal("something went wrong closing the db conn")
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(lesson); err != nil {
+		log.Fatal("error serializing lesson data to json")
+	}
+}
+
+// generateNewLesson deserializes a new Lesson from the request body and returns it to the caller.
+func generateNewLesson(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid Method", http.StatusBadRequest)
+		return
+	}
+
+	var lesson Lesson
+	json.NewDecoder(r.Body).Decode(&lesson)
+
+	fmt.Fprintf(w, "Received lesson of Id: %d, Title: %s, Description: %s", lesson.Id, lesson.Title, lesson.Description)
 }
 
 func main() {
 	router := mux.NewRouter()
 
 	// Register handlers
-	router.HandleFunc("/lessons", GetLessons).Methods("GET")
+	router.HandleFunc("/lessons", getLessons).Methods("GET")
+	router.HandleFunc("/lessons", generateNewLesson).Methods("POST")
+	router.HandleFunc("/lessons/{id}", getLessonById).Methods("GET")
 
-	http.ListenAndServe(":80", router)
+	log.Fatal(http.ListenAndServe(":80", router))
 }
